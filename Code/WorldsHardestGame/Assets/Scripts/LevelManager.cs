ad00d50931bc8e50643cabfdbe4199a19fbce662
juwho123 <114698027+Comet-Games@users.noreply.github.com>
2022-10-19 21:22:20 +0100

    Added 1.14

--- Code/WorldsHardestGame/Assets/Scripts/LevelManager.cs
+++ Code/WorldsHardestGame/Assets/Scripts/LevelManager.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 	public Text levelText;
 	public int levelNum = 1;
+	public int currentLevel = 0;
 	public int levelAmount;
 	public Player player;
 	public GameObject[] coins;
 	public Camera camera;
 	public GameObject EndScreen;
 
+	public GameObject optionsMenu;
+
 	void Start()
 	{
+		optionsMenu.SetActive(false);
 		levelAmount = player.levels.Length;
 	}
 	void Update()
@@ -26,8 +31,25 @@ public class LevelManager : MonoBehaviour {
 		{
 			EndScreen.SetActive(true);
 		}
-	}
-	public void ActivateCoins()
+
+		if(Input.GetButtonDown("Select") || (Input.GetButtonDown("Start")) && optionsMenu != true)
+		{
+			optionsMenu.SetActive(true);
+			Time.timeScale = 0;
+
+        }
+        if (Input.GetButtonDown("Select") || (Input.GetButtonDown("Start")) && optionsMenu != false)
+		{
+            optionsMenu.SetActive(false);
+			Time.timeScale = 1;
+        }
+		if(Input.GetButtonDown("Cross") && optionsMenu == true)
+		{
+            SceneManager.LoadScene(1);
+        }
+
+    }
+    public void ActivateCoins()
 	{
 		foreach(GameObject obj in coins)
 		{
--- Code/WorldsHardestGame/Assets/endScreen.cs
+++ Code/WorldsHardestGame/Assets/endScreen.cs
@@ -7,11 +7,6 @@ public class endScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetButton("Circle") || Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
 		if(Input.GetButton("Cross") || Input.GetKey(KeyCode.Return))
 		{
 			SceneManager.LoadScene(1);
